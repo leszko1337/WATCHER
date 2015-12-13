97a200c295176a6d729f7e45bbcdefb66d972586
leszko1337 <m.leszkovits96@htl-ottakring.ac.at>
2015-12-13 16:16:10 +0100

    Projekt hochgeladen

---
+++ Watcher5_1_1/wear/src/main/java/com/example/watcher5_1_1/watcher5_1_1/station.java
@@ -0,0 +1,14 @@
+package com.example.watcher5_1_1.watcher5_1_1;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class station extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_station);
+    }
+
+}
